6c989957134d0cb931b6fada6536beadb000f1e9
Jérémy Bolduc <BoldJe1.work@gmail.com>
2020-12-17 13:03:34 -0500

    Implemented fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+
+game  'gta5'
+
+author 'MiiMii1205'
+description 'A simple script for aiming teleportation'
+version '0.0.1'
+
+client_script 'Scripts/aimt.lua'
+export 'TeleportAtAimPoint'
