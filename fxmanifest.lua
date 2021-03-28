3a4bae162f0ddc6cb28db5f6b36ab600198d8713
Jérémy Bolduc <BoldJe1.work@gmail.com>
2021-03-28 13:18:13 -0400

    Added a repository field in the fxmanifest.lua file

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,6 +5,7 @@ game 'gta5'
 author 'MiiMii1205'
 description 'A short and sweet script to teleport yourself where you are aiming.'
 version '1.2.0'
+repository 'https://github.com/MiiMii1205/aim-teleporter'
 
 client_script 'Scripts/aimt.lua'
 export 'TeleportAtAimPoint'
